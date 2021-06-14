8e49369a54936699aca22fa48e8433a89ac447fe
a b <forpluralsight1900@gmail.com>
2021-06-14 15:13:55 -0500

    first cut of spring web from https://start.spring.io/

---
+++ src/main/java/com/abb/springbootfundamentals/SpringbootFundamentalsApplication.java
@@ -0,0 +1,13 @@
+package com.abb.springbootfundamentals;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootFundamentalsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootFundamentalsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/abb/springbootfundamentals/SpringbootFundamentalsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.abb.springbootfundamentals;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootFundamentalsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
