09378a2d27e05f8f46ce1e43df3494c1fc7f4ad0
a b <forpluralsight1900@gmail.com>
2021-06-14 15:36:37 -0500

    added debug logging and comment on @SpringBootApplication

--- src/main/java/com/abb/springbootfundamentals/SpringbootFundamentalsApplication.java
+++ src/main/java/com/abb/springbootfundamentals/SpringbootFundamentalsApplication.java
@@ -3,11 +3,13 @@ package com.abb.springbootfundamentals;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//@Configuration, @EnableAutoConfiguration, @ComponentScan
 @SpringBootApplication
 public class SpringbootFundamentalsApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringbootFundamentalsApplication.class, args);
+		System.out.println("Hola Spring Boot");
 	}
 
 }
